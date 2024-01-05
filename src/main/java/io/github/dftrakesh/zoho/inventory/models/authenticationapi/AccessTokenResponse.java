16d9de24b32c9f90261be8b9431e0460a47706cb
vivek hapani <vivek.hapani@dakshyafreondtech.com>
2024-01-05 12:19:41 +0530

    SYN-277 Created getItems and getItemById

---
+++ src/main/java/io/github/dftrakesh/zoho/inventory/models/authenticationapi/AccessTokenResponse.java
@@ -0,0 +1,15 @@
+package io.github.dftrakesh.zoho.inventory.models.authenticationapi;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Data;
+
+@Data
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class AccessTokenResponse {
+    private String scope;
+    private String tokenType;
+    private String apiDomain;
+    private Integer expiresIn;
+    private String accessToken;
+}
---
+++ src/main/java/io/github/dftrakesh/zoho/inventory/models/itemapi/ItemsWrapper.java
@@ -0,0 +1,14 @@
+package io.github.dftrakesh.zoho.inventory.models.itemapi;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ItemsWrapper {
+    private String code;
+    private String message;
+    private List<Item> items;
+}
